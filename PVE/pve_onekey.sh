dda9bac1e7ed8a6f3ed0cdfb867e3dad2ba945b3
roa <84313163+roacn@users.noreply.github.com>
2022-03-13 18:42:59 +0800

    Update pve_onekey.sh

--- PVE/pve_onekey.sh
+++ PVE/pve_onekey.sh
@@ -20,7 +20,7 @@ TIME() {
 }
 # PVE语言包设置
 pvelocale(){
-	sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && TIME g "PVE语言包设置完成!"
+	sed -i 's/# en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen && locale-gen && TIME g "PVE语言包设置完成!"
 }
 # apt国内源
 aptsources() {
